3a900289701a92150442d319c5b31a22588d1cfd
CJ Frontera <cjfrontera@gmail.com>
2014-05-29 10:31:58 -0400

    Added devise and changed application folders

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+#Required for hiroku. 
+#note to set this to actual host
+
 end
